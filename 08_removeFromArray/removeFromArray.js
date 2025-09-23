3c48b16f7d3fdf8c5186f7500f59aac90ef900b6
Eddie Gutman <edigutman@gmail.com>
2025-09-23 22:34:47 +0300

    08_removeFromArray Add solution

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...items) {
+  // const index = arr.indexOf(item);
+  // arr.splice(index, 1);
+  // return arr;
+
+  return arr.filter(el => !items.includes(el));
 };
 
 // Do not edit below this line
