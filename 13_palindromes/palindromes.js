aed5111a16d73fe51f0de44fc61ab2cda9d6c772
Eddie Gutman <edigutman@gmail.com>
2025-12-05 15:54:43 +0200

    13_palindromes Add solution

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const split = str.replace(/[^\w]/g, '').toLowerCase().split('');
+  const len = split.length;
 
+  for (let i = 0, j = len - 1; i < j; i++, j--) {
+    if (split[i] !== split[j]) {
+      return false;
+    }
+  }
+
+  return true;
 };
 
 // Do not edit below this line
