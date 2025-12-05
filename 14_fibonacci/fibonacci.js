53153dbe5756f7ce200d19ecd30e23d6e785d2ef
Eddie Gutman <edigutman@gmail.com>
2025-12-05 16:09:10 +0200

    14_fibonacci Add solution

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  const num = Number(n);
+  if (!Number.isFinite(num) || num < 0) {
+    return 'OOPS';
+  }
 
+  if (num === 0) {
+    return 0;
+  }
+
+  if (num === 1 || num === 2) {
+    return 1;
+  }
+
+  return fibonacci(num - 1) + fibonacci(num - 2);
 };
 
 // Do not edit below this line
