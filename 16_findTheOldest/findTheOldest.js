d56e246ade83ee8fc08b7c960b7ac2fbcf7de541
Eddie Gutman <edigutman@gmail.com>
2025-12-05 16:20:43 +0200

    16_findTheOldest Add solution

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  const sortByAge = people.sort((a, b) => getPersonAge(b) - getPersonAge(a));
+  return sortByAge[0];
 };
+
+function getPersonAge(person) {
+  if (person.yearOfDeath) {
+    return person.yearOfDeath - person.yearOfBirth;
+  } else {
+    const today = new Date();
+    const currentYear = today.getFullYear();
+    return currentYear - person.yearOfBirth;
+  }
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
